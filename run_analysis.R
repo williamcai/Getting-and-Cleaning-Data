71937002c78ea0634da067d33c11aaf47a582664
williamcai <caiwg2012@yeah.net>
2014-07-24 11:42:52 +0800

    Course Project

--- run_analysis.R
+++ run_analysis.R
@@ -8,20 +8,19 @@
 #   variable for each activity and each subject. 
 # ------------------------------------------------------------------------------
 
+dir <- c("./")
+
 # read activity labels
-#act_label <- read.table("UCI HAR Dataset/activity_labels.txt")
-act_label <- read.table("activity_labels.txt")
+act_label     <- read.table(paste(dir,"activity_labels.txt",sep = ""))
+
 # read features
-#feature_label <- read.table("UCI HAR Dataset/features.txt",stringsAsFactors = FALSE)
-feature_label <- read.table("features.txt",stringsAsFactors = FALSE)
+feature_label <- read.table(paste(dir,"features.txt",       sep=""),
+                            stringsAsFactors = FALSE)
 
 # read training data
-#train     <- read.table("UCI HAR Dataset/train/X_train.txt")
-#train_act <- read.table("UCI HAR Dataset/train/y_train.txt") 
-#train_sub <- read.table("UCI HAR Dataset/train/subject_train.txt")
-train     <- read.table("X_train.txt")
-train_act <- read.table("y_train.txt") 
-train_sub <- read.table("subject_train.txt")
+train     <- read.table(paste(dir,"X_train.txt",      sep = ""))
+train_act <- read.table(paste(dir,"y_train.txt",      sep = "")) 
+train_sub <- read.table(paste(dir,"subject_train.txt",sep = ""))
 
 # ------------------------------------------------------------------------------
 # 3.Uses descriptive activity names to name the activities in the data set
@@ -36,12 +35,9 @@ data1 <- data.frame(cbind(train, data[,2],train_sub))
 names(data1) <- c(feature_label[,2],"activity","subject")
 
 # read test data
-#test      <- read.table("UCI HAR Dataset/test/X_test.txt")
-#test_act  <- read.table("UCI HAR Dataset/test/y_test.txt") 
-#test_sub  <- read.table("UCI HAR Dataset/test/subject_test.txt")
-test      <- read.table("X_test.txt")
-test_act  <- read.table("y_test.txt") 
-test_sub  <- read.table("subject_test.txt")
+test      <- read.table(paste(dir,"X_test.txt",      sep = ""))
+test_act  <- read.table(paste(dir,"y_test.txt",      sep = ""))
+test_sub  <- read.table(paste(dir,"subject_test.txt",sep = ""))
 
 # ------------------------------------------------------------------------------
 # 3.Uses descriptive activity names to name the activities in the data set
@@ -69,6 +65,8 @@ subsetData <- data[,sub]
 subsetData$subject  <- data$subject
 subsetData$activity <- data$activity
 
+write.table(subsetData,file="tidyData.txt",sep=",",row.names = FALSE)
+
 # ------------------------------------------------------------------------------
 # 5.Creates a second, independent tidy data set with the average of each 
 #   variable for each activity and each subject. 
@@ -78,16 +76,15 @@ v_act <- data.frame()
 v_col <- ncol(subsetData)-2
 
 for (i in 1:v_col) {
-  v_sub <- rbind(v_sub,tapply(subsetData[,i],subsetData$subject,mean))
+  v_sub <- rbind(v_sub,tapply(subsetData[,i],subsetData$subject, mean))
   v_act <- rbind(v_act,tapply(subsetData[,i],subsetData$activity,mean))
   if (i == v_col) {
-    names(v_sub) <- names(tapply(subsetData[,i],subsetData$subject,mean))
+    names(v_sub) <- names(tapply(subsetData[,i],subsetData$subject, mean))
     names(v_act) <- names(tapply(subsetData[,i],subsetData$activity,mean))
   }
 }
-v_a <- cbind(feature=feature_label[,2],v_act)
-v_s <- cbind(feature=feature_label[,2],v_sub)
+v_a <- cbind(feature=feature_label[sub,2],v_act)
+v_s <- cbind(feature=feature_label[sub,2],v_sub)
 
 tidyData <- cbind(v_a,v_s[,2:ncol(v_s)])
-write.table(tidyData,file="tidyData.txt",sep=",",row.names = FALSE)
-# ------------------------------------------------------------------------------
+write.table(tidyData,file="secondTidyData.txt",sep=",",row.names = FALSE)
